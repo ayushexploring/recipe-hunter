a154c44b5be41e13a482993bdce2a8abb765b583
Ayush Verma <ayush.k1998@gmail.com>
2018-08-26 19:36:34 +0530

    Finished recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,12 @@
 class Recipe < ApplicationRecord
+  #key_value = "31c388c0c5f859983f4ea332128f6fd2"
+  key_value = ENV['FOOD2FORK_KEY']
+  hostport = ENV["FOOD2FORK_SERVER_AND_PORT"] || "www.food2fork.com"
+  base_uri "http://#{hostport}/api"
+  default_params key: key_value
+  format :json
+
+  def self.for (keyword)
+    get("/search", query: {q: keyword})["recipes"]
+  end
 end
