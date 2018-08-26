67a61bb8905abbbc5b11cc6ad7b2df98f45be8a5
Ayush Verma <ayush.k1998@gmail.com>
2018-08-26 19:33:57 +0530

    Finished recipes controller

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,4 +1,16 @@
-class RecipeController < ApplicationController
+class RecipesController < ApplicationController
   def index
+  	if !params[:search].nil?
+  	else
+  		params[:search] = ""
+  	end
+  	@recipes = Recipe.for(params[:search])
   end
 end
+
+#  class RecipesController < ApplicationController
+#   def index
+#     @search_term = params[:search] || "chocolate"
+#     @recipes_found = Recipe.for(@search_term)
+#   end
+# end
